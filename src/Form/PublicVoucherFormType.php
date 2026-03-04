df43c24cad3da8f2a756bc263536dd4f7a03647f
Артем Рябцев <artem@riabtsev.ru>
2026-03-04 15:29:57 +0300

    Настроить валидацию для поле ваучера

--- src/Form/PublicVoucherFormType.php
+++ src/Form/PublicVoucherFormType.php
@@ -5,9 +5,12 @@ namespace App\Form;
 use App\Entity\Voucher;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Validator\Constraints as Assert;
 
 class PublicVoucherFormType extends AbstractType
 {
@@ -17,9 +20,33 @@ class PublicVoucherFormType extends AbstractType
         $today = new \DateTime();
 
         $builder
-            ->add('fullName')
-            ->add('orcid')
-            ->add('email')
+            ->add('fullName', TextType::class, [
+                'label' => 'Full name',
+                'attr' => [
+                    'placeholder' => 'Иванов Иван Иванович',
+                    'pattern' => '[а-яА-ЯёЁa-zA-Z\s\-]+',  // Только буквы, пробелы, дефисы
+                    'minlength' => 5,
+                    'maxlength' => 255,
+                    'title' => 'ФИО должно содержать только буквы, пробелы и дефисы (минимум 5 символов)',
+                    'required' => true,
+                ],
+            ])
+            ->add('orcid', TextType::class, [
+                'label' => 'Orcid',
+                'attr' => [
+                    'placeholder' => '0000-0000-0000-0000',
+                    'pattern' => '\d{4}-\d{4}-\d{4}-\d{4}',
+                    'title' => 'ORCID должен быть в формате 0000-0000-0000-0000',
+                    'required' => true,
+                ],
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+                'attr' => [
+                    'placeholder' => 'name@example.com',
+                    'required' => true,
+                ],
+            ])
             ->add('type', ChoiceType::class, [
                 'label' => 'Type',
                 'choices' => [
